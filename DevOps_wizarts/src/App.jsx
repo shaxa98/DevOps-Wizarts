09f4160f4f7a434d4d4197a371b4cc8bec5af897
shaxa98 <shakhzodakhrorov@gmail.com>
2025-03-20 21:11:03 +0500

    update

--- DevOps_wizarts/src/App.jsx
+++ DevOps_wizarts/src/App.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import Heather from "./components/Heather/heather";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Heather />
+    </div>
+  );
 }
 
 export default App;
---
+++ DevOps_wizarts/src/components/Heather/heather.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./heather.css";
+function heather() {
+  return (
+    <div>
+      <button>hello </button>
+    </div>
+  );
+}
+
+export default heather;
