9b1f544a6f438a211d3e801d401f14576ded0c76
shaxa98 <shakhzodakhrorov@gmail.com>
2025-03-26 22:35:20 +0500

    update about.jsx

--- DevOps_wizarts/src/components/About/about.jsx
+++ DevOps_wizarts/src/components/About/about.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 import "./about.css";
+import AboutBackground from "../../assets/AboutBacgraund.svg";
 
 function About() {
   return (
-    <section className="about" style={{ border: "none" }}>
+    <section
+      className="about"
+      style={{
+        border: "none",
+        backgroundImage: `url(${AboutBackground})`,
+        backgroundSize: "contain", // Ensure the image fits within the screen
+        backgroundPosition: "left center", // Align the image to the left
+        backgroundRepeat: "no-repeat",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
       <h2>About Us</h2>
       <p>
         We are DevOps Wizards, dedicated to turning your ideas into reality with
