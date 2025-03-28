db85abdb38180c5431fb3747bf2c36f32325a363
shaxa98 <shakhzodakhrorov@gmail.com>
2025-03-28 20:10:45 +0500

    update

--- DevOps_wizarts/src/components/WhatIsDevOps/whatIsDevOps.jsx
+++ DevOps_wizarts/src/components/WhatIsDevOps/whatIsDevOps.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./whatIsDevOps.css";
 import WhatisDevopsBackground from "../../assets/WhatisDevopsBacgraund.svg";
+import WhatIsDevOpsImage from "../../assets/WhatIsDevOps.svg";
 
 function WhatIsDevOps() {
   return (
@@ -14,8 +15,49 @@ function WhatIsDevOps() {
         backgroundRepeat: "no-repeat",
         width: "100%",
         height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        padding: "20px",
       }}
-    ></section>
+    >
+      <img
+        src={WhatIsDevOpsImage}
+        alt="What is DevOps"
+        style={{
+          width: "50%",
+          height: "auto",
+          marginRight: "20px", // Add spacing between the image and text
+        }}
+      />
+      <div style={{ flex: 1, textAlign: "left" }}>
+        <h2 style={{ color: "blue", fontSize: "20px" }}>super interesting!</h2>
+        <h1 style={{ color: "white", fontSize: "50px" }}>What is DevOps?</h1>
+        <p style={{ color: "white", fontSize: "32px" }}>
+          DevOps is a practice that enhances collaboration between development
+          and operations teams, focusing on automation and integration to
+          improve software delivery.
+        </p>
+        <ul
+          style={{
+            color: "white",
+            fontSize: "32px",
+            listStyleType: "none",
+            padding: "0",
+          }}
+        >
+          <li>c Quicker release cycles with CI/CDQ</li>
+          <li>c Better teamwork between Dev and OpsQ</li>
+          <li>c Automation reduces errors and saves timeQ</li>
+          <li>c Automated testing ensures stable softwareQ</li>
+          <li>c Efficiently scales infrastructure and applicationsQ</li>
+          <li>c Integrates security practices from the start.</li>
+        </ul>
+        <p style={{ color: "orange", fontSize: "32px" }}>
+          DevOps accelerates and improves the software development process,
+          ensuring faster, more reliable, and secure releases.
+        </p>
+      </div>
+    </section>
   );
 }
 
