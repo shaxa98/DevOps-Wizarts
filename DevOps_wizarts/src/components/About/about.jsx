ab2208fc69115a126c463f433ee519343c5e1726
shaxa98 <shakhzodakhrorov@gmail.com>
2025-03-28 15:59:43 +0500

    update about.jsx

--- DevOps_wizarts/src/components/About/about.jsx
+++ DevOps_wizarts/src/components/About/about.jsx
@@ -15,7 +15,26 @@ function About() {
         width: "100%",
         height: "100vh",
       }}
-    ></section>
+    >
+      <div style={{ padding: "20px", textAlign: "left" }}>
+        <p style={{ color: "blue", fontSize: "32px" }}>Cool but who we are?</p>
+        <h2 style={{ color: "white", fontSize: "52px" }}>About DevOps</h2>
+        <p style={{ color: "white", fontSize: "32px" }}>
+          Welcome to DevOps Wizards, where we transform your ideas into reality
+          with cutting-edge technologies and skilled professionals. If you're
+          seeking automation for your business and IT processes, we're the
+          DevOps experts you need! At DevOps Wizards, we provide top-notch
+          automation services using the most popular and intuitive tools
+          available on the market.
+        </p>
+        <p style={{ color: "white", fontSize: "32px" }}>
+          DevOps Wizards is a team of experienced developers and engineers who
+          work on creating efficient and reliable business solutions. We strive
+          for innovation and continuous improvement to deliver the best results
+          for our clients.
+        </p>
+      </div>
+    </section>
   );
 }
 
