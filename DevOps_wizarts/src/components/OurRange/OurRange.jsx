b72dea4f75754662b3e36568a0c3557e47773119
shaxa98 <shakhzodakhrorov@gmail.com>
2025-03-28 22:47:57 +0500

    update

--- DevOps_wizarts/src/components/OurRange/OurRange.jsx
+++ DevOps_wizarts/src/components/OurRange/OurRange.jsx
@@ -1,17 +1,33 @@
 import React from "react";
 import backgroundImage from "../../assets/OurRangBacgraund.svg";
-
+import "./ourRange.css";
+import ButtonRightside from "../../assets/ButtonRightside.svg";
+import ButtonLeftside from "../../assets/ButtonLeftside.svg";
 function OurRange() {
   return (
     <div
       style={{
-        backgroundImage: `url(${backgroundImage})`,
+        // backgroundImage: `url(${backgroundImage})`,
+        backgroundColor: "red",
         backgroundSize: "cover",
         backgroundPosition: "center",
         width: "100%",
         height: "100vh",
       }}
-    ></div>
+    >
+      <div className="headerO">
+        <div>
+          <img className="button" src={ButtonLeftside} alt="" />
+          <img className="button" src={ButtonRightside} alt="" />
+        </div>
+        <h1>Our Range of services</h1>
+      </div>
+      <div className="container">
+        <div className="board">board1</div>
+        <div className="board">board2</div>
+        <div className="board">board3</div>
+      </div>
+    </div>
   );
 }
 
