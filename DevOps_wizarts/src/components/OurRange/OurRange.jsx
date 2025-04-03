e2459a0f7e5044f0b0179ead082346e8e827faa1
shaxa98 <shakhzodakhrorov@gmail.com>
2025-04-03 11:55:15 +0500

    update

--- DevOps_wizarts/src/components/OurRange/OurRange.jsx
+++ DevOps_wizarts/src/components/OurRange/OurRange.jsx
@@ -3,21 +3,20 @@ import backgroundImage from "../../assets/OurRangBacgraund.svg";
 import "./ourRange.css";
 import ButtonRightside from "../../assets/ButtonRightside.svg";
 import ButtonLeftside from "../../assets/ButtonLeftside.svg";
-import Icon1 from "../../assets/OurRange/01.svg";
-import Icon2 from "../../assets/OurRange/02.svg";
-import Icon3 from "../../assets/OurRange/03.svg";
-import Icon4 from "../../assets/OurRange/04.svg";
-import Icon5 from "../../assets/OurRange/05.svg";
-import Icon6 from "../../assets/OurRange/06.svg";
-import Icon7 from "../../assets/OurRange/07.svg";
-import Icon8 from "../../assets/OurRange/08.svg";
-import Icon9 from "../../assets/OurRange/09.svg";
+import Icon1 from "../../assets/OurRangeIcons/01.svg";
+import Icon2 from "../../assets/OurRangeIcons/02.svg";
+import Icon3 from "../../assets/OurRangeIcons/03.svg";
+import Icon4 from "../../assets/OurRangeIcons/04.svg";
+import Icon5 from "../../assets/OurRangeIcons/05.svg";
+import Icon6 from "../../assets/OurRangeIcons/06.svg";
+import Icon7 from "../../assets/OurRangeIcons/07.svg";
+import Icon8 from "../../assets/OurRangeIcons/08.svg";
+import Icon9 from "../../assets/OurRangeIcons/09.svg";
 function OurRange() {
   return (
     <div
       style={{
         backgroundImage: `url(${backgroundImage})`,
-
         backgroundSize: "cover",
         backgroundPosition: "center",
         width: "100%",
@@ -32,9 +31,29 @@ function OurRange() {
         <h1>Our Range of services</h1>
       </div>
       <div className="containerO">
-        <div className="board">board1</div>
-        <div className="board">board2</div>
-        <div className="board">board3</div>
+        <div className="board">
+          <div>
+            <div className="FirstLine">
+              <img src={Icon1} alt="" />
+              <h1>Infrastructure Monitoring</h1>
+            </div>
+          </div>
+          <p>Maintain optimal system performance with thorough monitoring.</p>
+        </div>
+        <div className="board">
+          <div style={{}}>
+            <img src={Icon2} alt="" />
+            <h1>Service 2</h1>
+          </div>
+          <p>Description for service 2.</p>
+        </div>
+        <div className="board">
+          <div>
+            <img src={Icon3} alt="" />
+            <h1>Service 3</h1>
+          </div>
+          <p>Description for service 3.</p>
+        </div>
       </div>
     </div>
   );
