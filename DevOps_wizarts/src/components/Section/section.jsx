66cbede2fcdd807d0ebdffe0e3db7b70c9f777a2
shaxa98 <shakhzodakhrorov@gmail.com>
2025-04-04 16:48:45 +0500

    update

--- DevOps_wizarts/src/components/Section/section.jsx
+++ DevOps_wizarts/src/components/Section/section.jsx
@@ -21,7 +21,34 @@ function Section() {
         </h1>
         <p>You can reach us anytime via contact@devops-wizards.eu</p>
       </div>
-      <div className="SectionRight"></div>
+      <div className="SectionRight">
+        <div className="input1">
+          <p>Name Surname</p>
+          <input type="text" placeholder="Your Full Name" class="input-field" />
+        </div>
+        <div className="input2">
+          <p>Email</p>
+          <input
+            type="text"
+            placeholder="your@company.com"
+            class="input-field"
+          />
+        </div>
+        <div className="input3">
+          <p>Phone number</p>
+          <input type="text" placeholder="+371 29999999" class="input-field" />
+        </div>
+        <div className="input4">
+          <p>Your message</p>
+          <input
+            type="text"
+            placeholder="Type your message here..."
+            class="input-field"
+          />
+        </div>
+
+        <button className="GetInTouch">Get in touch with us!</button>
+      </div>
     </div>
   );
 }
