8725832142de7e24c573bd4f01686fdf25698fff
shaxa98 <shakhzodakhrorov@gmail.com>
2025-04-04 18:01:26 +0500

    update some

--- DevOps_wizarts/src/components/Footer/footer.jsx
+++ DevOps_wizarts/src/components/Footer/footer.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import backgroundImage from "../../assets/FooterBacgraund.svg";
-
+import "./footer.css";
+import GitLogo from "../../assets/GitHubLogo.svg";
+import FacebookLogo from "../../assets/FacebookLogo.svg";
+import InstaLogo from "../../assets/InstagramLogo.svg";
+import TwitterLogo from "../../assets/TwitterLogo.svg";
 function footer() {
   return (
     <div
@@ -12,7 +16,14 @@ function footer() {
         height: "200px", // Adjust height as needed
       }}
     >
-      <div>footer</div>
+      <div>
+        footer
+        <div className="footerTop">
+          <div className="topleft"></div>
+          <div className="topRight"></div>
+        </div>
+        <div className="footerBottom"></div>
+      </div>
     </div>
   );
 }
